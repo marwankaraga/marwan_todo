d0edc3095236894d8344454f9744cac57d1d086d
marwan <mrwankaraga44@gmail.com>
2022-12-10 22:16:51 +0300

    sasasa

--- client/todos/src/App.js
+++ client/todos/src/App.js
@@ -16,7 +16,7 @@ function App() {
     e.preventDefault();
     try {
      
-      const res = await axios.post("https://mern-app-stacks-api/api/item", { item: itemText,numb:number})
+      const res = await axios.post("https://mern-app-stacks-apis.onrender.com", { item: itemText,numb:number})
       console.log(res);
     }
     catch (err) {
@@ -27,7 +27,7 @@ function App() {
   useEffect(() => {
     const getItem = async () => {
       try {
-        const res = await axios.get('http://mern-app-stacks-api/api/items')
+        const res = await axios.get('https://mern-app-stacks-apis.onrender.com')
         setListItem(res.data);
       } catch (err) {
      console.log(err);
